e8aa980de370678164f8af159e811ff112bb942d
Евгений Симонов <simonov.ev.al@gmail.com>
2020-11-01 13:29:24 +0300

    'lesson_4'

--- basketapp/models.py
+++ basketapp/models.py
@@ -3,12 +3,22 @@ from django.conf import settings
 from mainapp.models import Product
 
 
+# class BasketQuerySet(models.QuerySet):
+#
+#     def delete(self, *args, **kwargs):
+#         for object in self:
+#             object.product.quantity += object.quantity
+#             object.product.save()
+#         super(BasketQuerySet, self).delete(*args, **kwargs)
+
+
 class Basket(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket',
                              verbose_name='пользователь')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='продукт')
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+    # objects = BasketQuerySet.as_manager()
 
     class Meta:
         verbose_name = 'корзина'
@@ -34,3 +44,19 @@ class Basket(models.Model):
     def get_items(user):
         return Basket.objects.filter(user=user).order_by('product__category')
 
+    @staticmethod
+    def get_item(pk, **kwargs):
+        return Basket.objects.get(pk=pk)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.product.quantity -= self.quantity - self.__class__.get_item(self.pk).quantity
+        else:
+            self.product.quantity -= self.quantity
+        self.product.save()
+        super(self.__class__, self).save(*args, **kwargs)
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
